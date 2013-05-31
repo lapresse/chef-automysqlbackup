5b2b1ac5f03bc2638156ad6110c429eb7088d9fc
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-05-31 16:44:29 +0200

    fixed permissions of backup folder when insecure; bump version;

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "a.rosenhagen@ffuenf.de"
 license          "Apache 2.0"
 description      "installs/configures automysqlbackup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.4"
+version          "0.1.5"
 
 %w{ ubuntu }.each do |os|
 	supports os
