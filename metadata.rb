25a5d9b45bb89fda24bafd220f03ace523c3e852
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-07-21 18:02:52 +0200

    removed dependency of mysql cookbook so we can use it also with the percona flavor of mysql

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer_email "a.rosenhagen@ffuenf.de"
 license          "Apache 2.0"
 description      "installs/configures automysqlbackup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 
 %w{ ubuntu }.each do |os|
 	supports os
 end
 
-%w{ mysql }.each do |ressource|
-	depends ressource
+%w{ mysql percona }.each do |ressource|
+	recommends ressource
 end
 
 recipe "automysqlbackup::default", "installs/configures automysqlbackup"
