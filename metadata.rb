3a6a4911ac84ee6e877ebea0a36fd489671123ff
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-05-01 18:05:03 +0200

    add name attribute to make berkshelf happy

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "a.rosenhagen@ffuenf.de"
 license          "Apache 2.0"
 description      "installs/configures automysqlbackup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "automysqlbackup"
 version          "0.1.6"
 
 %w{ ubuntu }.each do |os|
